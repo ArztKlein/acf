138643728a322b2a8f67cfc318630879ccd3fc02
ArztKlein <52363453+ArztKlein@users.noreply.github.com>
2022-12-29 17:17:00 +1300

    package

--- acfile/__init__.py
+++ acfile/__init__.py
@@ -1,3 +1,3 @@
 from .section import Section
 from .acf import ACF
-from .read import read_string
+from .read import read_string, read_file, read_lines
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(name='acfile',
+      version='0.1',
+      description='File format allowing math operations, variables, and sections.',
+      author='ArztKlein',
+      author_email='',
+      url='https://github.com/ArztKlein/acf',
+      keywords=['file','format', 'configuration', 'filetype'],
+      setup_requires=['wheel'],
+      python_requires='>3.6.1',
+      packages=find_packages(),
+      include_package_data=True
+)
