70ffbf86f3f9c3c3301bbc5360625084557a7a52
ArztKlein <52363453+ArztKlein@users.noreply.github.com>
2022-12-30 13:25:20 +1300

    add sly requirement

--- setup.py
+++ setup.py
@@ -11,5 +11,8 @@ setup(name='acfile',
       setup_requires=['wheel'],
       python_requires='>3.6.1',
       packages=find_packages(),
-      include_package_data=True
+      include_package_data=True,
+      install_requires=[
+            'sly'
+      ]
 )
